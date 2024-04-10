a4b6044a11f839c916dd055d53e60ce15497d700
Mainul Hossain <138763613+Mainul21@users.noreply.github.com>
2024-04-10 14:43:25 +0600

    problem solved

--- First Program/Hello_openGL.py
+++ First Program/Hello_openGL.py
@@ -11,13 +11,7 @@ GRID_HEIGHT = 50
 WINDOW_WIDTH = GRID_SIZE * GRID_WIDTH
 WINDOW_HEIGHT = GRID_SIZE * GRID_HEIGHT
 SNAKE_INITIAL_LENGTH = 3
-UPDATE_INTERVAL = 200  # milliseconds
-mood_selection = int(input("Enter 0 for easy and 1 for hard mood: "))
-if mood_selection not in [0,1]:
-    sys.exit('NO GAME FOR YOU!!!')
-
-
-
+UPDATE_INTERVAL = 300  # milliseconds
 
 # Directions
 DIR_UP = 1
@@ -64,37 +58,29 @@ def generate_food():
 def check_collision():
     head = snake[0]
     # Check if snake hits the walls
-    if head[0] < 0 or head[0] >= GRID_WIDTH or head[1] < 0 or head[1] >= GRID_HEIGHT:
+    if (
+        head[0] < 0
+        or head[0] >= GRID_WIDTH
+        or head[1] < 0
+        or head[1] >= GRID_HEIGHT
+    ):
         return True
     # Check if snake hits itself
     if head in snake[1:]:
         return True
     return False
 
-
 def update_snake():
     global direction, snake, food, score, UPDATE_INTERVAL
     head = snake[0]
     if direction == DIR_UP:
-        if mood_selection == 0:
-            new_head = (head[0], (head[1] + 1) % GRID_HEIGHT)  
-        else:
-            new_head = (head[0], head[1] + 1)
+        new_head = (head[0], head[1] + 1)
     elif direction == DIR_DOWN:
-        if mood_selection == 0:
-            new_head = (head[0], (head[1] - 1) % GRID_HEIGHT)  
-        else:
-            new_head = (head[0], head[1] - 1)
+        new_head = (head[0], head[1] - 1)
     elif direction == DIR_LEFT:
-        if mood_selection == 0:
-            new_head = ((head[0] - 1) % GRID_WIDTH, head[1]) 
-        else:
-            new_head = (head[0] - 1, head[1])
+        new_head = (head[0] - 1, head[1])
     elif direction == DIR_RIGHT:
-        if mood_selection == 0:
-            new_head = ((head[0] + 1) % GRID_WIDTH, head[1]) 
-        else:
-            new_head = (head[0] + 1, head[1])
+        new_head = (head[0] + 1, head[1])
 
     # Check if snake eats food
     if new_head == food:
@@ -106,24 +92,10 @@ def update_snake():
         snake.pop()
         snake.insert(0, new_head)
 
-
-
 def display():
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     draw_snake()
     draw_food()
-    glRasterPos2f(10, 480)
-    for c in str(score):
-        glutBitmapCharacter(GLUT_BITMAP_9_BY_15, ord(c))
-    glRasterPos2f(450, 480)
-    if mood_selection == 0:
-        for c in 'easy':
-            glutBitmapCharacter(GLUT_BITMAP_9_BY_15, ord(c))
-    elif mood_selection == 1:
-        for c in 'hard':
-            glutBitmapCharacter(GLUT_BITMAP_9_BY_15, ord(c))
-    
-
     glutSwapBuffers()
 
 def keyboard(key, x, y):
